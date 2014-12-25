00a53458ed92b77f7c99e2e8f3db5e9879f00182
Kyle Anderson <kyle@xkyle.com>
2014-12-25 10:09:01 -0800

    Comment out packing sniffing debug for production

--- client_firmware/sniffer.ino
+++ client_firmware/sniffer.ino
@@ -1,8 +1,8 @@
 //callback that prints received packets to the serial port
 void packet_sniffer_callback(uint8_t srcmacaddr[6], byte ip[4], const char *data, word len) {
-  Serial.print("Got Sniffed Packet from: ");
-  ether.printIp(ip);
-  Serial.println();
+  //Serial.print("Got Sniffed Packet from: ");
+  //ether.printIp(ip);
+  //Serial.println();
   // If we got a packet, we need to reset the absense_timer so it can start counting down again
   absense_timer = millis();
   turn_my_house_on();
